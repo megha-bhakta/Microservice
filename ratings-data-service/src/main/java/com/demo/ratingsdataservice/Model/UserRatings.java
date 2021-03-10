16dd6efcd49d9d891af85ab7bc76178cb17d2523
Megha Bhakta <meghabhakta3@gmail.com>
2021-03-11 00:57:36 +0530

    Used RestTemplate to call Ratings API

---
+++ ratings-data-service/src/main/java/com/demo/ratingsdataservice/Model/UserRatings.java
@@ -0,0 +1,31 @@
+package com.demo.ratingsdataservice.Model;
+
+import java.util.List;
+
+public class UserRatings {
+	
+	private String userid;
+	private List<Rating> rating;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public List<Rating> getRating() {
+		return rating;
+	}
+	public void setRating(List<Rating> rating) {
+		this.rating = rating;
+	}
+	public UserRatings(String userid, List<Rating> rating) {
+		super();
+		this.userid = userid;
+		this.rating = rating;
+	}
+	public UserRatings() {
+		super();
+	}
+	
+
+}
